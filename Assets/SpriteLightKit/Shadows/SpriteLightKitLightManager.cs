82b6b791035374646017ddf33dd1142221a34a68
Mike <mike@prime31.com>
2015-09-13 12:02:32 -0700

    - added shadow support and a demo scene for it - renamed some shaders

---
+++ Assets/SpriteLightKit/Shadows/SpriteLightKitLightManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+namespace Prime31
+{
+	public class SpriteLightKitLightManager : MonoBehaviour
+	{
+		List<Vector3> _spriteLightPositions = new List<Vector3>();
+
+
+		void Awake()
+		{
+			var slk = FindObjectOfType<SpriteLightKit>();
+
+			var allGOs = FindObjectsOfType<GameObject>();
+			for( var i = 0; i < allGOs.Length; i++ )
+			{
+				if( ( slk.lightLayer.value & 1 << allGOs[i].layer ) != 0 )
+				{
+					var pos = allGOs[i].transform.position;
+					pos.z = 0f;
+					_spriteLightPositions.Add( pos );
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// returns the weighted average position of any lights within range or the original position passed in if everything
+		/// is further than maxDistance
+		/// </summary>
+		/// <returns>The nearest light.</returns>
+		/// <param name="position">Position.</param>
+		/// <param name="maxDistance">Max distance.</param>
+		public Vector3 getAffectedAverageLightPos( Vector3 position, float maxSqrDistance )
+		{
+			position.z = 0;
+
+			// we want the weighted average position of any lights that are close enough
+			var totalWeight = 0f;
+			var accumulatedPosition = Vector3.zero;
+			for( var i = 0; i < _spriteLightPositions.Count; i++ )
+			{
+				var sqrDistance = sqrDistanceBetweenVectors( position, _spriteLightPositions[i] );
+				if( sqrDistance < maxSqrDistance )
+				{
+					// weight should be greater for closer lights and less for further away
+					var weight = maxSqrDistance - sqrDistance;
+
+					// tally the total weight
+					totalWeight += weight;
+					accumulatedPosition += ( weight * _spriteLightPositions[i] );
+				}
+			}
+				
+			// if we have a totalWeight we need to take into account
+			if( totalWeight > 0 )
+			{
+				return accumulatedPosition * ( 1f / totalWeight );
+			}
+
+			return position;
+		}
+
+
+		public float sqrDistanceBetweenVectors( Vector3 a, Vector3 b )
+		{
+			return new Vector2( a.x - b.x, a.y - b.y ).sqrMagnitude;
+		}
+	}
+}
---
+++ Assets/SpriteLightKit/Shadows/SpriteLightKitShadow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Prime31
+{
+	public class SpriteLightKitShadow : MonoBehaviour
+	{
+		[Tooltip( "Maximum distance that a light can be from the sprite to still affect it" )]
+		public float maxLightSqrDistance = 100f;
+		[Tooltip( "The maximum offset from the sprite that the shadow can be" )]
+		public float maxShadowTranslation = 0.5f;
+		[Tooltip( "The averaged light distance is multiplied by this and it affects how far the shadow will offset from the sprite" )]
+		public float shadowDistanceMultiplier = 1f;
+
+		SpriteLightKitLightManager _slkLightManager;
+		Transform _transform;
+		Material _material;
+
+
+		void Awake()
+		{
+			_slkLightManager = FindObjectOfType<SpriteLightKitLightManager>();
+			_transform = gameObject.transform;
+			_material = GetComponent<SpriteRenderer>().material;
+		}
+
+
+		void Update()
+		{
+			// we dont want the z component to influence anything
+			var position = _transform.position;
+			position.z = 0f;
+
+			var nearestLightPosition = _slkLightManager.getAffectedAverageLightPos( position, maxLightSqrDistance );
+
+			//Debug.DrawLine( position, nearestLightPosition, Color.red, 0.1f );
+
+			var lightDistance = _slkLightManager.sqrDistanceBetweenVectors( position, nearestLightPosition );
+			var lightDir = ( position - nearestLightPosition ).normalized * lightDistance * shadowDistanceMultiplier;
+			lightDir /= maxLightSqrDistance;
+
+			_material.SetFloat( "_HorizontalTranslation", Mathf.Clamp( lightDir.x, -maxShadowTranslation, maxShadowTranslation ) );
+			_material.SetFloat( "_VerticalTranslation", Mathf.Clamp( lightDir.y, -maxShadowTranslation, maxShadowTranslation ) );
+		}
+	}
+}
